f2a89849ee7c90c4dd35d535ec36a3038d8a5c8d
David Sigalov <duff1k@inbox.ru>
2025-09-11 23:45:09 +0300

    Add homework 1

---
+++ homework1.py
@@ -0,0 +1,22 @@
+SONGS = [
+    ("Blinding Lights", "The Weeknd", "pop", "03:20"),
+    ("Levitating", "Dua Lipa", "pop", "03:23"),
+    ("Believer", "Imagine Dragons", "rock", "03:24"),
+    ("Do I Wanna Know?", "Arctic Monkeys", "indie", "04:32"),
+    ("bad guy", "Billie Eilish", "pop", "03:14"),
+    ("HUMBLE.", "Kendrick Lamar", "hip-hop", "02:57"),
+    ("Numb", "Linkin Park", "rock", "03:07"),
+    ("Nothing Else Matters", "Metallica", "metal", "06:28"),
+    ("Shape of You", "Ed Sheeran", "pop", "03:53"),
+    ("Lose Yourself", "Eminem", "hip-hop", "05:26"),
+    ("Smells Like Teen Spirit", "Nirvana", "rock", "05:01"),
+    ("Starboy", "The Weeknd", "pop", "03:50"),
+    ("Get Lucky", "Daft Punk", "electronic", "04:08"),
+    ("Sunset Lover", "Petit Biscuit", "electronic", "03:58"),
+    ("Time", "Hans Zimmer", "soundtrack", "04:35"),
+    ("Happier Than Ever", "Billie Eilish", "indie", "04:58"),
+    ("Circles", "Post Malone", "pop", "03:35"),
+    ("The Less I Know The Better", "Tame Impala", "indie", "03:36"),
+]
+
+
